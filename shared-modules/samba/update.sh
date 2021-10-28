f24bccf98b32965c3b7a869b834f940806db9869
tinywrkb <tinywrkb@gmail.com>
2021-10-28 17:55:28 +0300

    add shared-modules/samba

---
+++ shared-modules/samba/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+_FPID=samba
+
+[ -f ${_FPID}.json ] || { echo "Can't find ${_FPID}.json"; exit 1; }
+
+../../tools/cpan-updater
+flatpak-external-data-checker --edit-only ${_FPID}.json
+flatpak-external-data-checker --edit-only lib32-${_FPID}.json
